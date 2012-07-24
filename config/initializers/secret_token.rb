15a5551eab917178de2b9a47f82a6d9ec939250b
Erik Petersen <erik@telmate.com>
2012-07-23 17:16:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tygerfaux::Application.config.secret_token = 'bf4da548df3abe497b3b674b1f152ada9a8f1db946893b971bc9132f1b88b5c3322b10526d3c77977383218229efc29038ff1b7ec2c3f6b55876d438a1d7508c'
